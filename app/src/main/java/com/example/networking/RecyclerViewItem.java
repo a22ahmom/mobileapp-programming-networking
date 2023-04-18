63f96935dea80aef347cbc7fee48ea312a55eadd
Sahand Omarzai <a22ahmom@student.his.se>
2023-04-18 11:37:41 +0200

    Created a RecyclerViewItem java file to represent the items plus a RecyclerViewAdapter java file which consists of RecyclerViewAdapter method and a ViewHolder.

---
+++ app/src/main/java/com/example/networking/RecyclerViewItem.java
@@ -0,0 +1,19 @@
+package com.example.networking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RecyclerViewItem {
+
+    private String title;
+
+    public RecyclerViewItem(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+}
